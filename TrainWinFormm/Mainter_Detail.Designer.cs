c3bc9cea9fe4cab0150665b680492a81d8374288
Huy <l3lkeera@gmail.com>
2025-10-28 09:59:47 +0700

    Add project files.

---
+++ TrainWinFormm/Mainter_Detail.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace TrainWinFormm
+{
+    partial class frmMainter_Detail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvSanPham = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dgvCategories = new System.Windows.Forms.DataGridView();
+            this.btnLoad = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSanPham)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategories)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvSanPham
+            // 
+            this.dgvSanPham.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSanPham.Location = new System.Drawing.Point(6, 54);
+            this.dgvSanPham.Name = "dgvSanPham";
+            this.dgvSanPham.RowHeadersWidth = 62;
+            this.dgvSanPham.RowTemplate.Height = 28;
+            this.dgvSanPham.Size = new System.Drawing.Size(734, 470);
+            this.dgvSanPham.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dgvCategories);
+            this.groupBox1.Controls.Add(this.dgvSanPham);
+            this.groupBox1.Location = new System.Drawing.Point(3, 343);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1491, 538);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông Tin Sản Phẩm";
+            // 
+            // dgvCategories
+            // 
+            this.dgvCategories.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCategories.Location = new System.Drawing.Point(744, 54);
+            this.dgvCategories.Name = "dgvCategories";
+            this.dgvCategories.RowHeadersWidth = 62;
+            this.dgvCategories.RowTemplate.Height = 28;
+            this.dgvCategories.Size = new System.Drawing.Size(734, 470);
+            this.dgvCategories.TabIndex = 0;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoad.Location = new System.Drawing.Point(600, 94);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(294, 177);
+            this.btnLoad.TabIndex = 2;
+            this.btnLoad.Text = "LOAD";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // frmMainter_Detail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1493, 879);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmMainter_Detail";
+            this.Text = "Hiển Thị Dữ Liệu";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSanPham)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategories)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvSanPham;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgvCategories;
+        private System.Windows.Forms.Button btnLoad;
+    }
+}
+
---
+++ TrainWinFormm/Mainter_Detail.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrainWinFormm
+{
+    public partial class frmMainter_Detail : Form
+    {
+        string connection = "Data Source=Huy\\SQLEXPRESS;Initial Catalog=QUANLYBANHANG;Integrated Security=True;Trust Server Certificate=True";
+        DataSet DataSet = new DataSet();
+        public frmMainter_Detail()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connection))
+            {
+                string truyvanDanhMuc = "SELECT COTID FROM CATAGORIES WHERE ";
+                SqlDataAdapter categories_Adapter = new SqlDataAdapter(truyvanDanhMuc,conn);
+
+
+            }
+        }
+    }
+}
